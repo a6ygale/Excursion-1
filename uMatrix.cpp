a4288c15ee25d405bcb336eee92c9de00bf80cdd
a6ygale <155013023+a6ygale@users.noreply.github.com>
2025-03-06 18:10:16 -0800

    Add files via upload

---
+++ uMatrix.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include "functions.h"
+using namespace std;
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//  uMatrix requires a 2D vector input 
+//  it returns a 1D array with voltage magnitudes of corresponding elements
+//  or a 0 in the case that the element is not a voltage supply
+///////////////////////////////////////////////////////////////////////////////////////////
+
+vector<double> uMatrix(vector<vector<double>> Melements){
+    vector<double> uM = {}; //creates an empty vector to store u values
+
+    for (int i=0; i<Melements.size(); i++){
+        //if the element is a voltage source add value
+        if (Melements[i][0]==0){
+            uM.push_back(Melements[i][3]); 
+        }
+        //if not push a zero
+        else{
+            uM.push_back(0); 
+        }
+    }
+    return uM; 
+}
